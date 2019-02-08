383efdbd97794b3dc187ff763f33bb91d8c80685
Bingkun Li <libingkun1997@gmail.com>
2019-02-08 08:30:26 +0000

    update

--- eQTL/eQTL-preprocess.py
+++ eQTL/eQTL-preprocess.py
@@ -13,13 +13,17 @@ def reform_eQTL(infile, out_file):
     # outfile2: bed format of merged intersections
     
     eQTL_raw = pd.read_table(infile, delim_whitespace=True)
+    
+    # keep only eQTL-TSS pairs with a tss_distance of at least 10kb
     eQTL_raw = eQTL_raw[eQTL_raw["tss_distance"].abs() > 10000]
     eQTL_raw["chr"] = "chr" + eQTL_raw["variant_id"].str.split("_", expand=True)[0]
     eQTL_raw["end"] = eQTL_raw["variant_id"].str.split("_", expand=True)[1]
     eQTL_raw["start"] = eQTL_raw["end"].apply(int) - eQTL_raw["tss_distance"]
+    # adjust order so "start" is always smaller than "end" -- prevent bedtools error 
     eQTL_raw["start"],eQTL_raw["end"] = np.where(eQTL_raw["start"] > eQTL_raw["end"].apply(int), 
         [eQTL_raw["end"],eQTL_raw["start"]], [eQTL_raw["start"], eQTL_raw["end"]])
     eQTL_reformed = eQTL_raw[["chr","start","end","gene_id","pval_nominal"]]
+    # add unique ID for each pair
     eQTL_ID = [i+1 for i in range(eQTL_reformed.shape[0])]  
     eQTL_reformed["ID"] = eQTL_ID
     eQTL_reformed.to_csv(out_file, sep="\t", index=False)
